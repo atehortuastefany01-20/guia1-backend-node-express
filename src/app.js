16b3656ac67fa04b43df5e1a9395fe5604fca611
Stefany Atehortua <atehortuastefany01@gmail.com>
2026-07-23 22:56:52 -0500

    Agregar ruta de saludo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("Servidor backend iniciado");
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/saludo', (req, res) => {
+  res.json({
+    mensaje: 'Bienvenido al backend con Node.js y Express'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
